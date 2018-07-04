2c4b36737844bb036ff9fa66f1c615a221c4b517
Duanpham <duanphamhvbc@gmail.com>
2018-07-04 21:21:01 +0700

    upup

--- NoGunny/src/warplane/WarPlane.java
+++ NoGunny/src/warplane/WarPlane.java
@@ -22,7 +22,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
-import org.junit.runner.JUnitCore;
+//import org.junit.runner.JUnitCore;
 
 import pkg2dgamesframework.AFrameOnImage;
 import pkg2dgamesframework.Animation;
